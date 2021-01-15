ae5e8bc7eee85d76e5d1f8edebb980a9b696fa0d
Daniel Goldsmith <aa9863@coventry.ac.uk>
2021-01-15 10:46:49 +0000

    Add some test data

---
+++ coding/buildData.py
@@ -0,0 +1,49 @@
+import json
+import random
+import string
+
+STRLEN = 10
+
+flag = "CUEH{testflag}"
+
+data = {}
+checksum = []
+names = ["dan", "james", "terry", "tono", "john"]
+
+import pprint
+
+for idx, item in enumerate(names):
+    print("X {} N {}".format(idx, item))
+
+    #Some random values
+    values = [random.randrange(10) for x in range(5)]
+    total = sum(values)
+
+    #Take the first of our random values and make it the checksum
+    selected = values[0]
+    checksum.append(selected)
+
+    theText = [random.choice(string.ascii_letters) for x in range(STRLEN)]
+
+    theText[selected] = flag[idx]
+
+    theUser = {"name": item,
+               "values": values,
+               "total": total,
+               "text": "".join(theText)
+    }
+
+    data[idx] = theUser
+    #pprint.pprint(theUser)
+
+
+output = {"dataset" : "testdata",
+          "checksum": checksum,
+          "data": data
+        }
+
+
+with open("testdata.json", "w") as fd:
+    out = json.dump(output, fd, sort_keys=True, indent=4)
+
+
