e85d2fa757fd4f66accaed291f5267cd85392c81
James Shuttleworth <digehode@googlemail.com>
2021-02-01 17:06:30 +0000

    Added week 1 runme.sh

---
+++ runners/runme.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+echo "                ____"
+echo "            ,dP9CGG88@b,"
+echo "          ,IP""YICCG888@@b,"
+echo "         dIi   ,IICGG8888@b"
+echo "        dCIIiciIICCGG8888@@b"
+echo "        GCCIIIICCCGGG8888@@@"
+echo "        GGCCCCCCCGGG88888@@@"
+echo "        GGGGCCCGGGG88888@@@@..."
+echo "        Y8GGGGGG8888888@@@@P....."
+echo "         Y88888888888@@@@@P......"
+echo "          Y8888888@@@@@@@P'......"
+echo "            .@@@@@@@@@P'......."
+echo "                """"........"
+echo "fl"
+
