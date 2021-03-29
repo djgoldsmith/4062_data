0175677ce0ccaa8522d11222735ceac0affce3db
Dan Goldsmith <aa9863@coventry.ac.uk>
2021-03-29 19:07:22 +0100

    HTML added

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -1,18 +1,18 @@
 import base64
 
-FILENAME = "../ascii_art/raptorBMX.txt"
+FILENAME = "../ascii_art/games_can_kill.txt"
 OUTFILE = "target.sh"
 
 outArray = ["echo '\n'"]
 
-idx = 100
+idx = 1000
 with open(FILENAME,"r") as fd:
     for line in fd:
         theStr = "{0} {1}".format(idx, line)
         encoded = base64.b64encode(theStr.encode()).decode()
         outArray.append("echo {0} | base64 -d".format(encoded))
 
-        idx +=1
+        idx -=1
 
 #outArray.reverse()
 import random
